7f2a0fb817978722aa354fcdc1dd86a36cb725bf
Suan Tonsing <suanasdf@gmail.com>
2020-10-14 15:01:57 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello Expense Manager App.js</Text>
+    </SafeAreaView>
+  );
+}
