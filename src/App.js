ad0d9d4437aa729bdc4af34b72fcf7c8703971b9
Suan Tonsing <suanasdf@gmail.com>
2020-10-14 20:58:22 +0530

    Added Nav drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Button, StatusBar} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+
+//my components & Screens
+import DrawerContent from './navigation/DrawerContent';
+import HomeStack from './navigation/HomeStack';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hello Expense Manager App.js</Text>
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Drawer.Navigator
+            initialRouteName="Dashboard"
+            drawerContent={(props) => <DrawerContent {...props} />}>
+            <Drawer.Screen name="Dashboard" component={HomeStack} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </>
   );
 }
